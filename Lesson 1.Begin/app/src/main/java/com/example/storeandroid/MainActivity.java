85c0cb27e1d52bd4198c5765ab2367647c5d5c47
novakvova <novakvova@gmail>
2023-10-16 16:12:46 +0300

    Begin working Add button and EditText

--- Lesson 1.Begin/app/src/main/java/com/example/storeandroid/MainActivity.java
+++ Lesson 1.Begin/app/src/main/java/com/example/storeandroid/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.storeandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText txtInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtInfo=findViewById(R.id.txtInfo);
+    }
+
+    public void onClickHandler(View view) {
+        String text = txtInfo.getText().toString();
+        Log.d("my-tag","-----Нажали кнопку------"+ text);
     }
 }
